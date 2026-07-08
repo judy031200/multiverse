029548d57216eda3bb406f69b58498affadc5fb6
judy031200 <hoaiak2004@gmail.com>
2026-07-08 11:15:29 +0700

    Add files via upload

---
+++ assets/css/morning_note.js
@@ -0,0 +1,58 @@
+body{
+    margin:0;
+    font-family:Arial;
+}
+
+header{
+
+display:flex;
+
+justify-content:space-between;
+
+padding:20px;
+
+background:#e8b11c;
+
+}
+
+.container{
+
+display:grid;
+
+grid-template-columns:70% 30%;
+
+gap:20px;
+
+padding:20px;
+
+}
+
+.left section{
+
+margin-bottom:30px;
+
+}
+
+.right{
+
+border-left:2px solid #ddd;
+
+padding-left:20px;
+
+}
+
+.up{
+
+color:#1ca55c;
+
+font-weight:bold;
+
+}
+
+.down{
+
+color:#d93025;
+
+font-weight:bold;
+
+}
